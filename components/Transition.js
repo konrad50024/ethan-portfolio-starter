56c0d0b2038b0763dc69c94f43103c9ae6e23276
konrad50024 <konrad50024@hotmail.com>
2024-01-13 18:51:46 +0000

    latest

--- components/Transition.js
+++ components/Transition.js
@@ -1,7 +1,47 @@
+import { motion } from "framer-motion";
+
+const transitionVariants = {
+  initial: {
+    x: "100%",
+    width: "100%",
+  },
+  animate: {
+    x: "0%",
+    width: "0%",
+  },
+  exit: {
+    x: ["0%", "100%"],
+    width: ["0%", "100%"],
+  },
+};
+
 const Transition = () => {
   return (
     <>
-      <div>Transition</div>
+      <motion.div
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#342763]"
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.2, duration: 1.4, ease: "easeInOut" }}
+      ></motion.div>
+      <motion.div
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#4b3694]"
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.4, duration: 1.8, ease: "easeInOut" }}
+      ></motion.div>
+      <motion.div
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#4b3792]"
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.6, duration: 0.4, ease: "easeInOut" }}
+      ></motion.div>
     </>
   );
 };
